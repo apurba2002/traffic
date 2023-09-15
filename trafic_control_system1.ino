2c49393b14cbe62527911997b38b34612ea84986
apurba2002 <debnathapurba582@gmail.com>
2023-09-15 19:24:59 +0600

    First commit

---
+++ trafic_control_system1.ino
@@ -0,0 +1,23 @@
+void setup()
+{
+  pinMode(4, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(2, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(4, HIGH); // For green LED
+  delay(5000); // Wait for 3 second
+  digitalWrite(4, LOW);
+
+  
+  digitalWrite(3, HIGH); // For orange LED
+  delay(2000);  // Wait for 1 second
+  digitalWrite(3, LOW);
+  
+  
+  digitalWrite(2, HIGH);  // For the red LED
+  delay(5000); // Wait for 3 second
+  digitalWrite(2, LOW); 
+}
